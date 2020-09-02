869ba06c5d721dca31057cbc7e428ffdfd71e964
Horacio Hernández <lachoscancunsoft@gmail.com>
2020-09-02 09:34:25 -0500

    Fin de la seccion 4 - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+//IMPORTACION DE MODULOS
+
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+//IMPORTACION DE COMPONENTES
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 const routes: Routes = [
-  {
-    path:'', 
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'grafica1', component: Grafica1Component},
-      {path: '', redirectTo: '/dashboard', pathMatch:'full'},
-    ]
-  },
-
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
-
+  {path: '', redirectTo:'/dashboard', pathMatch:'full'},
   {path: '**', component: NopagefoundComponent},
 ];
 
@@ -35,6 +22,10 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes)
   ],
-  exports: [RouterModule]
+  exports: [
+    RouterModule,
+    PagesRoutingModule,
+    AuthRoutingModule
+  ]
 })
 export class AppRoutingModule { }
