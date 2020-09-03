102c96a725fc2353360ee20efe63903415b48a26
Horacio Hernández <lachoscancunsoft@gmail.com>
2020-09-03 12:38:16 -0500

    Se creo un sidebar.service para el manejo de los menus

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SidebarService } from '../../services/sidebar.service';
+
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -8,7 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  menuItems: any[];
+
+  constructor( private sidebarService: SidebarService ) { 
+
+    this.menuItems = this.sidebarService.menu;
+    console.log(this.menuItems);
+
+
+  }
 
   ngOnInit(): void {
   }
